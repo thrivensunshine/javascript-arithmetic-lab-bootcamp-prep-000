84aeb02b461cfb1790c22059637d57f359c7ee63
DANIELLE ALBA <danib.sgh@gmail.com>
2019-01-31 18:42:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function divide(a,b){
 }
 
 function inc(n){
-  return n++;
+  return n+= 1;
 }
 
 function dec(n){
-  return n--;
+  return n-=1;
 }
 
 function makeInt(n){
@@ -27,5 +27,5 @@ function makeInt(n){
 }
 
 function preserveDecimal(n){
-  return parseFloat(n)
+  return parseFloat(n,10)
 }
